f665957ee0afc833c9a502545f8456c2945bc0a1
Andrew <andrewonulak@gmail.com>
2023-02-23 14:03:57 -0700

    commit tree fa2f26a3785a1df49fbbf384ea70028ad2fe3846

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,33 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, 'application/json'
+
   get '/' do
     { message: "Hello world" }.to_json
   end
+
+  get '/games' do
+    games = Game.all.order(:title)
+    games.to_json
+  end
+
+  get '/games_with_reviews' do
+    games = Game.all.order(:title)
+    games.to_json(include: :reviews)
+  end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+    game.to_json(include: { reviews: { include: :user } })
+  end
+
+  # get '/games/:id' do
+  #   game = if params[:id] == 'rand'
+  #     Game.find(rand(1..50))
+  #   else
+  #     Game.find(params[:id])
+  #   end
+  #   game.to_json
+  # end
 
 end
